49620bd50a80ab66e1d3f1b4981c2dc846f84a4a
ru.elric <ru.elric@5b4f1a2a-fed2-11de-9adb-2bc960bfb704>
2011-02-26 13:45:00 +0000

    ! RingWidget now uses ui file.

--- trunk/src/RingWidget.cpp
+++ trunk/src/RingWidget.cpp
@@ -1,47 +1,41 @@
 #include "RingWidget.h"
+#include <QApplication>
+#include <QDesktopWidget>
 #include <cstdlib> /* for system(). */
 #include "Alarm.h"
 #include "Sound.h"
 #include "SnoozeDialog.h"
+#include "MainWidget.h"
+#include "ui_RingWidget.h"
 
-RingWidget::RingWidget(QSettings* settings, boost::shared_ptr<Alarm> alarm, MainWidget* mainWidget): mSettings(settings), mAlarm(alarm), mMainWidget(mainWidget), mSnoozeSecs(0) {
-  QTextEdit* textMessage = new QTextEdit();
-  textMessage->setReadOnly(true);
-  textMessage->setPlainText(alarm->message());
+RingWidget::RingWidget(QSettings *settings, boost::shared_ptr<Alarm> alarm, MainWidget *mainWidget, QWidget *parent): 
+  QWidget(parent),
+  mUi(new Ui::RingWidget),
+  mSettings(settings), 
+  mAlarm(alarm), 
+  mMainWidget(mainWidget), 
+  mSnoozeSecs(0) 
+{
+  mUi->setupUi(this);
 
-  QPushButton *snooze5Button = new QPushButton("&Snooze 5 mins...");
-  connect(snooze5Button, SIGNAL(clicked()), this, SLOT(snooze5()));
-
-  mMoreButton = new QPushButton("&more...");
-  connect(mMoreButton, SIGNAL(clicked()), this, SLOT(more()));
-
-  QHBoxLayout *buttonsLayout = new QHBoxLayout();
-  buttonsLayout->addStretch(1);
-  buttonsLayout->addWidget(snooze5Button);
-  buttonsLayout->addWidget(mMoreButton);
-
-  QVBoxLayout* mainLayout = new QVBoxLayout();
-  mainLayout->addWidget(textMessage);
-  mainLayout->addLayout(buttonsLayout);
-
-  setLayout(mainLayout);
+  mUi->textEdit->setPlainText(alarm->message());
 
   mMoreMenu = new QMenu(this);
-  mMoreMenu->addAction("Snooze 10 minutes...", this, SLOT(snooze10()));
-  mMoreMenu->addAction("Snooze 15 minutes...", this, SLOT(snooze15()));
-  mMoreMenu->addAction("Snooze 30 minutes...", this, SLOT(snooze30()));
-  mMoreMenu->addAction("Snooze 60 minutes...", this, SLOT(snooze60()));
-  mMoreMenu->addAction("More...", this, SLOT(snoozeDialog()));
+  mMoreMenu->addAction(tr("Snooze 10 minutes..."), this, SLOT(snooze10()));
+  mMoreMenu->addAction(tr("Snooze 15 minutes..."), this, SLOT(snooze15()));
+  mMoreMenu->addAction(tr("Snooze 30 minutes..."), this, SLOT(snooze30()));
+  mMoreMenu->addAction(tr("Snooze 60 minutes..."), this, SLOT(snooze60()));
+  mMoreMenu->addAction(tr("More..."), this, SLOT(snoozeDialog()));
 
-  Sound* sound = new Sound(alarm->fileName(), this);
+  Sound *sound = new Sound(alarm->fileName(), this);
   sound->play();
 
   if(!alarm->commandLine().isEmpty())
-    system(alarm->commandLine().toAscii().constData());
+    system(alarm->commandLine().toAscii().constData()); /* TODO: use QProcess. */
 
   setWindowTitle(alarm->name());
 
-  resize(settings->value(keySize(), QSize(320, 100)).toSize());
+  resize(settings->value(keySize(), size()).toSize());
   move(settings->value(keyPos(), QApplication::desktop()->screenGeometry().center() - QPoint(320, 100) / 2).toPoint());
 }
 
@@ -62,7 +56,7 @@ void RingWidget::closeEvent(QCloseEvent *event) {
   event->accept();
 }
 
-void RingWidget::snooze5() {
+void RingWidget::on_snooze5Button_clicked() {
   mSnoozeSecs = 5 * 60;
   close();
 }
@@ -97,6 +91,6 @@ void RingWidget::snoozeDialog() {
   delete dlg;    
 }
 
-void RingWidget::more() {
-  mMoreMenu->popup(mapToGlobal(mMoreButton->pos() + QPoint(mMoreButton->width(), 0)));
+void RingWidget::on_moreButton_clicked() {
+  mMoreMenu->popup(mapToGlobal(mUi->moreButton->pos() + QPoint(mUi->moreButton->width(), 0)));
 }
